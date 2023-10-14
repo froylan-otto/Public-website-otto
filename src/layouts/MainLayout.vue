bc463da8803dbeedad23087a4c8802246d96d583
Cristian <cristian@ottomate.me>
2023-10-14 18:48:49 -0500

    adapt-header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,32 @@
 <template>
   <q-layout view="lHh Lpr lFf" style="background: #e3f6f5">
-    <q-header class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title align="center" class="text-bold">
-          OTTO LIGHT
-        </q-toolbar-title>
+    <q-header
+      class="no-shadow heading bg-primary q-py-lg"
+      bordered
+      style="height: 100px"
+    >
+      <q-toolbar class="col-4 flex justify-between">
+        <q-avatar size="60px">
+          <img
+            src="~/assets/imagesHome/otto-newlogo2013.png"
+            class="image-circle"
+          />
+        </q-avatar>
+        <q-tabs align="top" class="q-mr-xl" v-model="tab">
+          <q-route-tab
+            :to="{ name: 'tourPage' }"
+            replace
+            name="home"
+            label="Home"
+          />
+          <q-route-tab :to="{ name: 'tourPage' }" replace label="About us" />
+          <q-route-tab :to="{ name: 'tourPage' }" replace label="Register" />
+          <q-route-tab :to="{ name: 'loginPage' }" label="SIGN IN" />
+          <!-- <q-route-tab :to="{ name: 'signup' }" label="SIGN UP" /> -->
+          <!-- <q-route-tab :to="{ name: 'registration' }" label="REGISTRATION" /> -->
+        </q-tabs>
       </q-toolbar>
     </q-header>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -23,7 +42,9 @@ export default defineComponent({
   setup() {
     const $q = useQuasar();
 
-    return {};
+    return {
+      tab: ref("home"),
+    };
   },
   methods: {},
 });
